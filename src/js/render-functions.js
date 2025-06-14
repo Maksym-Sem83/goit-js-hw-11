15b5fcaa6ee4ab600dad4204e944d83d378e32b8
Maksym Semenov <max_semenov@ukr.net>
2025-06-14 18:01:37 +0300

    add main.js and add styles

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,7 +3,7 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gallery = document.querySelector(".gllery");
+const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 const lightbox = new SimpleLightbox(".gallery", { captionsData: "alt", captionDelay: 250, });
 
@@ -11,17 +11,29 @@ export const createGallery = images => {
     const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
     <li class="gallery-item">
     <a href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}"
+    <img src="${webformatURL}" alt="${tags}"/>
     </a>
     <div class="info">
-    <p>${likes}</p>
-    <p>${views}</p>
-    <p>${comments}</p>
-    <p>${downloads}</p>
+    <p class="properties">Likes <span class="value">${likes}</span></p>
+    <p class="properties">Views <span class="value">${views}</span></p>
+    <p class="properties">Comments <span class="value">${comments}</span></p>
+    <p class="properties">Downloads <span class="value">${downloads}</span></p>
     </div>
 
     </li>`).join("");
 
     gallery.insertAdjacentHTML("beforeend", markup);
     lightbox.refresh();
+};
+
+export const clearGallery = () => {
+    gallery.innerHTML = "";
+};
+
+export const showLoader = () => {
+    loader.classList.remove("hidden");
+};
+
+export const hideLoader = () => {
+    loader.classList.add("hidden");
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,44 @@
 import { getImagesByQuery } from "./js/pixabay-api";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
 const form = document.querySelector(".form");
 const input = form.elements["search-text"];
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const query = input.value.trim();
+    if (!query) {
+        iziToast.warning({
+            message: "Please enter a search term!",
+            position: "topRight",
+        });
+        return;
+    };
+
+showLoader();
+clearGallery();
+
+getImagesByQuery(query)
+    .then(data => {
+        if (!data.hits.length) {
+            iziToast.info({
+                message: "Sorry, there are no images matching your search query. Please try again!",
+                position: "topRight",
+            });
+            return;
+        };
+        createGallery(data.hits);
+    })
+    .catch(() => {
+        iziToast.error({
+            message: "Oops! Something went wrong. Please try again later.",
+            position: "topRight"
+        });
+    })
+    .finally(() => { hideLoader(); });
+});
